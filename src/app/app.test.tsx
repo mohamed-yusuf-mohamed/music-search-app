1875cf7fc2db46307a4b0a58362c61aa52c38b08
Mohamed Yusuf Mohamed <mohamed-yusuf-mohamed@outlook.com>
2021-07-26 09:37:44 +0100

    Tidy

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store } from '../redux/store';
 import App from './app';
 import userEvent from '@testing-library/user-event'
 
